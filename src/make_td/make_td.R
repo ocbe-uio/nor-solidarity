3a744f5d233c11b5d75d90b97fc42d0bffdf06be
ingeco <ingeco@medisin.uio.no>
2020-11-03 21:58:14 +0100

    Update make_td.R

--- src/make_td/make_td.R
+++ src/make_td/make_td.R
@@ -67,37 +67,37 @@ tdme <- raw %>%
 ##############
 
 tdran <- bind_rows(pick(raw,"ran"), pick(raw,"ran123"), pick(raw,"ran13"), pick(raw, "who")) %>% 
+  mutate(ranavail_rem = if_else( !is.na(whoarms3) | 
+                                   !is.na(ran123dt) |
+                                   !is.na(ran13dt), "Yes", "No")) %>% 
+  mutate(ranavail_hcq = if_else( !is.na(whoarms2cd) | 
+                                   !is.na(ran123dt) |
+                                   !is.na(randt), "Yes", "No")) %>%
   mutate(rantrt = case_when(
     !is.na(whotrt) ~ whotrt,
     !is.na(rantrt) ~ rantrt,
     !is.na(rantrt123) ~ rantrt123,
     !is.na(rantrt13) ~ rantrt13
   )) %>% 
+ 
   mutate(randt = case_when(
     !is.na(whorandt) ~ whorandt,
     !is.na(randt) ~ randt,
     !is.na(ran123dt) ~ ran123dt,
     !is.na(ran13dt) ~ ymd(ran13dt)
   )) %>% 
-  mutate(ranavail = case_when(
-    !is.na(whorandt) ~ str_split(glue("Standard of care (SOC);{whoarms2};{whoarms3}"),";"),
-    !is.na(randt) ~ list(c("Standard of care (SOC)", "Hydroxychloroquine + SOC")),
-    !is.na(ran123dt) ~ list(c("Standard of care (SOC)", "Hydroxychloroquine + SOC", "Remdesivir + SOC")),
-    !is.na(ran13dt) ~ list(c("Standard of care (SOC)", "Remdesivir + SOC"))
-  )) %>% 
-  mutate(ranavail_rem = if_else( !is.na(whoarms3))
-    !is.na(whoarms3) ~ "Yes",
-    !is.na(ran123dt) ~ "Yes",
-    !is.na(ran13dt) ~  "Yes")) %>% 
-  mutate(ranavail_hcq = case_when(
-    !is.na(whoarms2) ~ "Yes",
-    !is.na(ran123dt) ~ "Yes",
-    !is.na(randt) ~  "Yes")) %>% 
-  mutate(across(starts_with("ranavail_"), ~ factor(.x, levels = c("No", "Yes", ordered = TRUE)))) %>% 
+  
+
+  # mutate(ranavail_hcq = case_when(
+  #   !is.na(whoarms2) ~ "Yes",
+  #   !is.na(ran123dt) ~ "Yes",
+  #   !is.na(randt) ~  "Yes")) %>% 
+  mutate(across(starts_with("ranavail_"), ~ factor(.x, levels = c("No", "Yes"), ordered = TRUE))) %>% 
   select(subjectid, rantrt, randt, starts_with("ranavail")) %>% 
-  labelled::set_variable_labels(ranavail = "Available treatments",
-                                ranavail_rem = "Remdesivir available?",
-                                ranavail_hcq = "Hydroxychloroquine available?")
+  labelled::set_variable_labels(ranavail_rem = "Remdesivir available?",
+                                ranavail_hcq = "Hydroxychloroquine available?") %>% 
+  arrange(randt)
+
 
 
 write_rds(tdran, "data/td/tdran.rds")
